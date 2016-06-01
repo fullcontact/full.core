fullcontact/full.async

Version 0.3.1-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.async "0.3.0"
+(defproject fullcontact/full.async leiningen.release/bump-version
   :description "Extensions and helpers for core.async."
 
   :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]

fullcontact/full.core

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.core "0.3.0"
+(defproject fullcontact/full.core leiningen.release/bump-version
   :description "FullContact's core Clojure library - logging, configuration and common helpers."
     
   :dependencies [[org.clojure/tools.cli "0.3.1"]