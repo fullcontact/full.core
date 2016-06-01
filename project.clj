fullcontact/full.rabbit

Version 0.4.1-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.rabbit "0.4.0"
+(defproject fullcontact/full.rabbit "0.4.1-SNAPSHOT"
   :description "RabbitMQ sugar on top of langohr."
 
   :dependencies [[com.novemberain/langohr "3.0.0-rc2"]

fullcontact/full.core

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.core "0.4.0"
+(defproject fullcontact/full.core "0.4.1-SNAPSHOT"
   :description "FullContact's core Clojure library - logging, configuration and common helpers."
     
   :dependencies [[org.clojure/tools.cli "0.3.1"]