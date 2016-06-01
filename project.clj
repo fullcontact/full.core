fullcontact/full.async

full.monty parent module

project.clj
@@ -1,13 +1,6 @@
-(defproject fullcontact/full.async "0.2.7-SNAPSHOT"
+(defproject fullcontact/full.async "0.3.0-SNAPSHOT"
   :description "Extensions and helpers for core.async."
-  :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo}
-  :url "https://github.com/fullcontact/full.async"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :plugins [[lein-midje "3.1.3"]
-            [lein-release "1.0.5"]]
-  :lein-release {:deploy-via :shell
-                 :shell ["lein" "deploy" "clojars"]}
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+
+  :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+
+  :plugins [[lein-modules "0.3.11"]])

fullcontact/full.core

project.clj
@@ -1,17 +1,10 @@
-(defproject fullcontact/full.core "0.1.5-SNAPSHOT"
+(defproject fullcontact/full.core "0.3.0-SNAPSHOT"
   :description "FullContact's core Clojure library - logging, configuration and common helpers."
-  :url "https://github.com/fullcontact/full.core"
-  :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.3.1"]
+    
+  :dependencies [[org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.3.0"]
                  [me.moocar/logback-gelf "0.10p1"]
                  [ch.qos.logback/logback-classic "1.1.2"]
                  [clj-yaml "0.4.0"]]
-  :lein-release {:deploy-via :shell
-                 :shell ["lein" "deploy" "clojars"]}
-  :plugins [[lein-midje "3.1.3"]
-            [lein-release "1.0.5"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+
+  :plugins [[lein-modules "0.3.11"]])