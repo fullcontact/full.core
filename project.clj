e030d9dc313ede3e84f775e9ef8786932cbd8861
Kaspars Dancis <kaspars@fullcontact.com>
2017-03-23 14:27:10 -0600

    Version 1.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.core "1.0.3-SNAPSHOT"
+(defproject fullcontact/full.core "1.0.3"
   :description "FullContact's core Clojure(Script) library - logging, configuration and sugar."
   :url "https://github.com/fullcontact/full.core"
   :license {:name "Eclipse Public License - v 1.0"
